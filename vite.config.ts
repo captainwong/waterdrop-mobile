97edb49bd545fbfa3bbe94ca152e7220eeb0b64c
i7-8700 <1281261856@qq.com>
2023-09-21 23:04:18 +0800

    fix vite-plugin-eslint ts7016

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve('./src') },
+    ]
+  },
 })
